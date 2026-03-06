ad1586cf420ce9ac3a323b662d84cf3b39f5bc6d
Nmreinoso <nmreinoso@wm.edu>
2026-03-06 04:59:43 -0500

    Fix site URL formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: ''https://Nmreinoso.github.io',
+  site: 'https://Nmreinoso.github.io',
   base: '/personal-site',
 });
